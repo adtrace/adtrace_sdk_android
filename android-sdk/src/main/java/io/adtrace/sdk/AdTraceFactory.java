8bb55bbf09341b557b5fd38689b623a8ceb0920f
Nasser Amini <namini40@gmail.com>
2022-10-23 15:35:44 +0330

    update imports in `AdTraceFactory`

--- android-sdk/src/main/java/io/adtrace/sdk/AdTraceFactory.java
+++ android-sdk/src/main/java/io/adtrace/sdk/AdTraceFactory.java
@@ -2,8 +2,9 @@ package io.adtrace.sdk;
 
 import android.content.Context;
 
+import io.adtrace.sdk.network.IActivityPackageSender;
+import io.adtrace.sdk.network.UtilNetworking;
 import java.net.URL;
-
 import javax.net.ssl.HttpsURLConnection;
 
 import io.adtrace.sdk.network.IActivityPackageSender;
