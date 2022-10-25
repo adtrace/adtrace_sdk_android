ef7fcd493533633199b20441dc70d56940a22b47
Nasser Amini <namini40@gmail.com>
2022-10-25 13:07:04 +0330

    add third party sharing method

--- android-sdk-plugin-webbridge/src/main/assets/adtrace_third_party_sharing.js
+++ android-sdk-plugin-webbridge/src/main/assets/adtrace_third_party_sharing.js
@@ -1,10 +1,17 @@
 function AdTraceThirdPartySharing(isEnabled) {
     this.isEnabled = isEnabled;
     this.granularOptions = [];
+    this.partnerSharingSettings = [];
 }
 
 AdTraceThirdPartySharing.prototype.addGranularOption = function(partnerName, key, value) {
     this.granularOptions.push(partnerName);
     this.granularOptions.push(key);
     this.granularOptions.push(value);
+};
+
+AdTraceThirdPartySharing.prototype.addPartnerSharingSetting = function(partnerName, key, value) {
+    this.partnerSharingSettings.push(partnerName);
+    this.partnerSharingSettings.push(key);
+    this.partnerSharingSettings.push(value);
 };
