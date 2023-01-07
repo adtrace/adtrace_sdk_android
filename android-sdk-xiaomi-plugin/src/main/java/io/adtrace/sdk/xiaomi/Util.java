ed3dd8d6ce288940cbccbf762be4440293dd4920
Nasser Amini <namini40@gmail.com>
2023-01-07 16:50:39 +0330

    add `android-sdk-xiaomi-plugin`

---
+++ android-sdk-xiaomi-plugin/src/main/java/io/adtrace/sdk/xiaomi/Util.java
@@ -0,0 +1,27 @@
+package io.adtrace.sdk.xiaomi;
+
+import android.content.Context;
+
+import io.adtrace.sdk.ILogger;
+import io.adtrace.sdk.ReferrerDetails;
+import com.miui.referrer.api.GetAppsReferrerDetails;
+
+public class Util {
+   public synchronized static ReferrerDetails getXiaomiInstallReferrerDetails(Context context, ILogger logger) {
+      if (!AdTraceXiaomiReferrer.shouldReadXiaomiReferrer) {
+         return null;
+      }
+
+      GetAppsReferrerDetails getAppsReferrerDetails = XiaomiReferrerClient.getReferrer(context, logger, 3000);
+      if (getAppsReferrerDetails == null) {
+         return null;
+      }
+
+      return new ReferrerDetails(getAppsReferrerDetails.getInstallReferrer(),
+                                 getAppsReferrerDetails.getReferrerClickTimestampSeconds(),
+                                 getAppsReferrerDetails.getInstallBeginTimestampSeconds(),
+                                 getAppsReferrerDetails.getReferrerClickTimestampServerSeconds(),
+                                 getAppsReferrerDetails.getInstallBeginTimestampServerSeconds(),
+                                 getAppsReferrerDetails.getInstallVersion(), null);
+   }
+}
