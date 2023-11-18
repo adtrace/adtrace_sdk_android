811e176620f5dcb741339587232333715b865732
Nasser Amini <namini40@gmail.com>
2023-11-18 19:07:47 +0330

    update samsung referrer

--- android-sdk-samsung-plugin/src/main/java/io/adtrace/sdk/samsung/Util.java
+++ android-sdk-samsung-plugin/src/main/java/io/adtrace/sdk/samsung/Util.java
@@ -5,14 +5,13 @@ import android.content.Context;
 import io.adtrace.sdk.ILogger;
 import io.adtrace.sdk.ReferrerDetails;
 
-
 public class Util {
    public synchronized static ReferrerDetails getSamsungInstallReferrerDetails(Context context, ILogger logger) {
       if (!AdTraceSamsungReferrer.shouldReadSamsungReferrer) {
          return null;
       }
 
-      com.sec.android.app.samsungapps.installreferrer.api.ReferrerDetails referrerDetails =
+      com.samsung.android.sdk.sinstallreferrer.api.ReferrerDetails referrerDetails =
               SamsungReferrerClient.getReferrer(context, logger, 3000);
       if (referrerDetails == null) {
          return null;
